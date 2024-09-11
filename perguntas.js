66dd62c4986fc9350a14cb4ab61a4fbcdf7d7aaf
LZ_RJ777 <152527068+luizmrmd@users.noreply.github.com>
2024-09-11 14:48:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,38 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'luiz roberto',
+    'nome da mãe de luiz?',
+    'janailda'
+)
+
+criaCartao(
+    'luiz roberto',
+    'qual é a data de aniversario de luiz?',
+    '24/11/2007'
+)
+
+criaCartao(
+    
+    'luiz roberto',
+    'idade de luiz?',
+    '16 anos '
+
+)
+
+criaCartao(
+    'luiz roberto',
+    ''
+
+
+)
