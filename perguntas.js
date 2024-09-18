4f07c64ab8ce4b415d997753f91adc60f04fc554
LZ_RJ777 <152527068+luizmrmd@users.noreply.github.com>
2024-09-18 14:29:16 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -31,8 +31,36 @@ criaCartao(
 )
 
 criaCartao(
-    'luiz roberto',
-    ''
+    'CIENCIA',
+    ' Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?',
+    'Tem entre 4 a 6 litros. São retirados 450 mililitros',
 
+)
+
+criaCartao( 
+    'historia',
+    ' De quem é a famosa frase “Penso, logo existo”?',
+    'descartes',
+
+
+)
+
+criaCartao(
+    'historia',
+    'De onde é a invenção do chuveiro elétrico?',
+    `Brasil`,
+)
+
+criaCartao(
+        `Geografia`,
+        `Quais o menor e o maior país do mundo?`, 
+        `Vaticano e Rússia`,
+        
+)
+
+criaCartao(
+         `Historia`,
+         ` Qual o nome do presidente do Brasil que ficou conhecido como Jango?`,   
+         `João Goulart`, 
 
 )
