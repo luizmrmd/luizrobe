672665487c2fec562520a205f4206ede574d06ba
LZ_RJ777 <152527068+luizmrmd@users.noreply.github.com>
2024-09-18 14:32:36 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -11,34 +11,34 @@ criaCartao(
 )
 
 criaCartao(
-    'luiz roberto',
+    'Luiz roberto',
     'nome da mãe de luiz?',
-    'janailda'
+    'Janailda'
 )
 
 criaCartao(
-    'luiz roberto',
+    'Luiz roberto',
     'qual é a data de aniversario de luiz?',
     '24/11/2007'
 )
 
 criaCartao(
     
-    'luiz roberto',
+    'Luiz roberto',
     'idade de luiz?',
     '16 anos '
 
 )
 
 criaCartao(
-    'CIENCIA',
+    'Ciencia',
     ' Normalmente, quantos litros de sangue uma pessoa tem? Em média, quantos são retirados numa doação de sangue?',
     'Tem entre 4 a 6 litros. São retirados 450 mililitros',
 
 )
 
 criaCartao( 
-    'historia',
+    'Historia',
     ' De quem é a famosa frase “Penso, logo existo”?',
     'descartes',
 
@@ -46,7 +46,7 @@ criaCartao(
 )
 
 criaCartao(
-    'historia',
+    'Historia',
     'De onde é a invenção do chuveiro elétrico?',
     `Brasil`,
 )
